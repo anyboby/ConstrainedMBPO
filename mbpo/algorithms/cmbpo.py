10485877bd22ca86833dc3222bbf159665bd07b6
anyboby <zanger.moritz@googlemail.com>
2020-05-28 00:17:33 +0200

    omg this initial exploration

--- mbpo/algorithms/cmbpo.py
+++ mbpo/algorithms/cmbpo.py
@@ -529,9 +529,9 @@ class CMBPO(RLAlgorithm):
                 " n_initial_exploration_steps > 0.")
 
         self.sampler.initialize(env, initial_exploration_policy, pool)
-        while pool.size < self._n_initial_exploration_steps:
+        while pool.arch_size < self._n_initial_exploration_steps:
             self.sampler.sample()
-            if self.sampler.batch_ready:
+            if self.ready_to_train:
                 self.sampler.finish_all_paths(append_val=True)
                 pool.dump_to_archive() # move old policy samples to archive
 
