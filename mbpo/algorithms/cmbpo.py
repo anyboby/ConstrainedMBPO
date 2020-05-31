35181205b3d8870d0366f2aea71f4c466a356749
anyboby <zanger.moritz@googlemail.com>
2020-05-31 19:02:01 +0200

    added separate training batch sizes for dyn model and cost model

--- mbpo/algorithms/cmbpo.py
+++ mbpo/algorithms/cmbpo.py
@@ -364,15 +364,17 @@ class CMBPO(RLAlgorithm):
                                                         'rewards',
                                                         'costs',
                                                         'terminals'])
-                if len(samples['observations'])>15000:
-                    samples = {k:v[-15000:] for k,v in samples.items()} 
+                if len(samples['observations'])>25000:
+                    dyn_samples = {k:v[-25000:] for k,v in samples.items()} 
+                if len(samples['observations'])>10000:
+                    cost_samples = {k:v[-10000:] for k,v in samples.items()} 
     
                 #self.fake_env.reset_model()    # this behaves weirdly
                 min_epochs = 10 if self._epoch==0 else 0        ### overtrain a little in the beginning to jumpstart uncertainty prediction
                 max_epochs = 500 if self._epoch<10 else 15
                 if self._epoch%self._dyn_model_train_freq==0:
                     model_train_metrics_dyn = self.fake_env.train_dyn_model(
-                        samples, 
+                        dyn_samples, 
                         batch_size=512, 
                         max_epochs=max_epochs, 
                         min_epoch_before_break=min_epochs, 
@@ -383,7 +385,7 @@ class CMBPO(RLAlgorithm):
 
                 if self._epoch%self._cost_model_train_freq==0 and self.fake_env.cares_about_cost:
                     model_train_metrics_cost = self.fake_env.train_cost_model(
-                        samples, 
+                        cost_samples, 
                         batch_size=512, 
                         min_epoch_before_break=min_epochs,
                         max_epochs=max_epochs, 
