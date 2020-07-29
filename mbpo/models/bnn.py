added a variance corrected NLL loss to NN model, not sure if this is sound

mbpo/models/bnn.py
@@ -101,7 +101,7 @@ class BNN:
 
     @property
     def is_probabilistic(self):
-        return True if self.loss_type == 'NLL' else False
+        return True if 'NLL' in self.loss_type else False
 
     @property
     def is_tf_model(self):
@@ -221,6 +221,11 @@ class BNN:
                 self.sy_train_targ = tf.placeholder(dtype=tf.float32,
                                                 shape=[self.num_nets, None, self.layers[-1].get_output_dim() // 2],
                                                 name="training_targets")
+                
+                if self.loss_type == 'NLL_varcorr':
+                    self.var_corr_targ = tf.placeholder(dtype=tf.float32,
+                                                shape=[self.num_nets, None, self.layers[-1].get_output_dim() // 2],
+                                                name="training_targets")
             else:
                 self.sy_train_targ = tf.placeholder(dtype=tf.float32,
                                                 shape=[self.num_nets, None, self.layers[-1].get_output_dim()],
@@ -232,7 +237,7 @@ class BNN:
                                                 name="weighted_loss")
             else:
                 self.weights=None
-
+            
             #### set up losses
             if self.loss_type == 'NLL':
                 train_loss = tf.reduce_sum(self._nll_loss(self.sy_train_in, self.sy_train_targ, inc_var_loss=True, weights=self.weights))
@@ -241,6 +246,13 @@ class BNN:
                 self.loss = self._nll_loss(self.sy_train_in, self.sy_train_targ, inc_var_loss=False, weights=self.weights)
                 self.tensor_loss, self.debug_mean = self._nll_loss(self.sy_train_in, self.sy_train_targ, inc_var_loss=False, tensor_loss=True, weights=self.weights)            
             
+            if self.loss_type == 'NLL_varcorr':
+                train_loss = tf.reduce_sum(self._nll_loss(self.sy_train_in, self.sy_train_targ, inc_var_loss=True, weights=self.weights, var_correction=self.var_corr_targ))
+                train_loss += tf.add_n(self.decays)
+                train_loss += 0.01 * tf.reduce_sum(self.max_logvar) - 0.01 * tf.reduce_sum(self.min_logvar)
+                self.loss = self._nll_loss(self.sy_train_in, self.sy_train_targ, inc_var_loss=False, weights=self.weights)
+                self.tensor_loss, self.debug_mean = self._nll_loss(self.sy_train_in, self.sy_train_targ, inc_var_loss=False, tensor_loss=True, weights=self.weights)            
+
             elif self.loss_type == 'MSE':
                 train_loss = tf.reduce_sum(self._nll_loss(self.sy_train_in, self.sy_train_targ, inc_var_loss=False, weights=self.weights))
                 train_loss += tf.add_n(self.decays)
@@ -447,6 +459,10 @@ class BNN:
             weights = kwargs['weights']
             weights, holdout_weights = weights[permutation[num_holdout:]], weights[permutation[:num_holdout]]
             holdout_weights = np.tile(holdout_weights[None], [self.num_nets, 1])
+        if self.loss_type == 'NLL_varcorr':
+            var_corr_tar = kwargs['var_corr']
+            var_corr_tar, holdout_var_corr_tar = var_corr_tar[permutation[num_holdout:]], var_corr_tar[permutation[:num_holdout]]
+            holdout_var_corr_tar = np.tile(holdout_var_corr_tar[None], [self.num_nets, 1])
 
         print(f'[ {self.name} ] Training {inputs.shape} | Holdout: {holdout_inputs.shape}')
 
@@ -483,7 +499,8 @@ class BNN:
                     feed_dict.update({self.old_pred_ph:old_pred[batch_idxs]})
                 if self.weights is not None:
                     feed_dict.update({self.weights:weights[batch_idxs]})
-
+                if self.loss_type == 'NLL_varcorr':
+                    feed_dict.update({self.var_corr_targ:var_corr_tar[batch_idxs]})
                 self.sess.run(
                     self.train_op,
                     feed_dict=feed_dict
@@ -499,6 +516,8 @@ class BNN:
                     }
                 if self.weights is not None:
                     progress_feed_dict.update({self.weights:weights[idxs[:, :max_logging]]})
+                if self.loss_type == 'NLL_varcorr':
+                    progress_feed_dict.update({self.var_corr_targ:var_corr_tar[idxs[:, :max_logging]]})
 
                 if holdout_ratio < 1e-12:
                     feed_dict={
@@ -818,7 +837,7 @@ class BNN:
         else:
             return mean
 
-    def _nll_loss(self, inputs, targets, inc_var_loss=True, tensor_loss=False, weights=None):
+    def _nll_loss(self, inputs, targets, inc_var_loss=True, tensor_loss=False, weights=None, var_correction=None):
         """Helper method for compiling the NLL loss function.
 
         The loss function is obtained from the log likelihood, assuming that the output
@@ -851,11 +870,25 @@ class BNN:
             inv_var = tf.exp(-log_var)
 
             if weights is not None:
-                mse_losses = tf.reduce_mean(weights * tf.reduce_mean(0.5 * tf.square(mean - targets) * inv_var, axis=-1), axis=-1)
-                var_losses = tf.reduce_mean(weights * tf.reduce_mean(0.5 * log_var, axis=-1), axis=-1) 
+                if var_correction is not None:
+                    var_corr = tf.clip_by_value(tf.exp(log_var)-var_correction, 1e-10, 1e6) 
+                    inv_var_corr = 1/var_corr
+                    log_var_corr = tf.log(var_corr)
+                    mse_losses = tf.reduce_mean(weights * tf.reduce_mean(0.5 * tf.square(mean - targets) * inv_var_corr, axis=-1), axis=-1)
+                    var_losses = tf.reduce_mean(weights * tf.reduce_mean(0.5 * log_var_corr, axis=-1), axis=-1) 
+                else:
+                    mse_losses = tf.reduce_mean(weights * tf.reduce_mean(0.5 * tf.square(mean - targets) * inv_var, axis=-1), axis=-1)
+                    var_losses = tf.reduce_mean(weights * tf.reduce_mean(0.5 * log_var, axis=-1), axis=-1) 
             else:
-                mse_losses = tf.reduce_mean(tf.reduce_mean(0.5 * tf.square(mean - targets) * inv_var, axis=-1), axis=-1)
-                var_losses = tf.reduce_mean(tf.reduce_mean(0.5 * log_var, axis=-1), axis=-1) 
+                if var_correction is not None:
+                    var_corr = tf.clip_by_value(tf.exp(log_var)-var_correction, 1e-10, 1e6) 
+                    inv_var_corr = 1/var_corr
+                    log_var_corr = tf.log(var_corr)
+                    mse_losses = tf.reduce_mean(tf.reduce_mean(0.5 * tf.square(mean - targets) * inv_var_corr, axis=-1), axis=-1)
+                    var_losses = tf.reduce_mean(tf.reduce_mean(0.5 * log_var_corr, axis=-1), axis=-1) 
+                else:
+                    mse_losses = tf.reduce_mean(tf.reduce_mean(0.5 * tf.square(mean - targets) * inv_var, axis=-1), axis=-1)
+                    var_losses = tf.reduce_mean(tf.reduce_mean(0.5 * log_var, axis=-1), axis=-1) 
 
             self.var_loss_deb = var_losses
             total_losses = mse_losses + var_losses

softlearning/policies/cpo_policy.py
@@ -509,8 +509,8 @@ class CPOPolicy(BasePolicy):
             # Organize placeholders for zipping with data from buffer on updates
             # careful ! this has to be in sync with the output of our buffer !
             self.buf_fields = [
-                self.obs_ph, self.a_ph, self.adv_ph,
-                self.cadv_ph, self.ret_ph, self.cret_ph,
+                self.obs_ph, self.a_ph, self.adv_ph, 'ret_var',
+                self.cadv_ph, 'cret_var', self.ret_ph, self.cret_ph,
                 self.logp_old_ph, self.old_v_ph, self.old_vc_ph,
                 self.cur_cost_ph
                 ] + values_as_sorted_list(self.pi_info_phs)
@@ -527,7 +527,9 @@ class CPOPolicy(BasePolicy):
             self.critic_phs = [
                 self.obs_ph, 
                 self.ret_ph, 
+                'ret_var',
                 self.cret_ph, 
+                'cret_var',
                 self.old_v_ph, 
                 self.old_vc_ph,
                 ]
@@ -757,7 +759,26 @@ class CPOPolicy(BasePolicy):
                 vc_targets,
                 old_pred = old_vc,
                 **kwargs,
-                )                                            
+                )                        
+        elif self.vf_loss == 'NLL_varcorr':
+            ret_var = inputs['ret_var'][..., None]
+            cret_var = inputs['cret_var'][..., None]
+            v_kwargs = kwargs.copy()
+            v_kwargs['var_corr'] = ret_var
+            vc_kwargs = kwargs.copy()
+            vc_kwargs['var_corr'] = cret_var
+
+            v_metrics = self.v.train(
+                obs_in,
+                v_targets,
+                **v_kwargs,
+                )                                      
+
+            vc_metrics = self.vc.train(
+                obs_in,
+                vc_targets,
+                **vc_kwargs,
+                )                        
         else:
             v_metrics = self.v.train(
                 obs_in,
@@ -801,7 +822,7 @@ class CPOPolicy(BasePolicy):
                                             inputs[self.cret_ph][:, np.newaxis]
 
         v_loss = self.v.validate(v_ins, v_tars)
-        vc_loss = self.vc.validate(vc_ins, vc_tars)        
+        vc_loss = self.vc.validate(vc_ins, vc_tars)
         
         critic_metric = dict()
         critic_metric['Loss' + self.v.name] = v_loss

softlearning/replay_pools/cpobuffer.py
@@ -41,12 +41,12 @@ class CPOBuffer:
             'actions':      self.act_buf,
             'next_observations': self.nextobs_buf,
             'advantages':   self.adv_buf,
-            'advantage_vars': self.adv_var_buf,
+            'return_vars': self.ret_var_buf,
             'rewards':      self.rew_buf,
             'returns':      self.ret_buf,
             'values':       self.val_buf,
             'cadvantages':  self.cadv_buf,
-            'cadvantage_vars':  self.cadv_var_buf,
+            'creturn_vars':  self.cret_var_buf,
             'costs':        self.cost_buf,
             'creturns':     self.cret_buf,
             'cvalues':      self.cval_buf,
@@ -60,12 +60,12 @@ class CPOBuffer:
             'actions':      self.act_archive,
             'next_observations': self.nextobs_archive,
             'advantages':   self.adv_archive,
-            'advantage_vars': self.adv_var_archive,
+            'return_vars': self.ret_var_archive,
             'rewards':      self.rew_archive,
             'returns':      self.ret_archive,
             'values':       self.val_archive,
             'cadvantages':  self.cadv_archive,
-            'cadvantage_vars':  self.cadv_var_archive,
+            'creturn_vars':  self.cret_var_archive,
             'costs':        self.cost_archive,
             'creturns':     self.cret_archive,
             'cvalues':      self.cval_archive,
@@ -94,14 +94,14 @@ class CPOBuffer:
         self.act_buf = np.zeros(combined_shape(size, self.act_shape), dtype=np.float32)
         self.nextobs_buf = np.zeros(combined_shape(size, self.obs_shape), dtype=np.float32)          
         self.adv_buf = np.zeros(size, dtype=np.float32)
-        self.adv_var_buf = np.zeros(size, dtype=np.float32)
+        self.ret_var_buf = np.zeros(size, dtype=np.float32)
         self.roll_lengths_buf = np.zeros(size, dtype=np.float32)
         self.rew_buf = np.zeros(size, dtype=np.float32)
         self.val_buf = np.zeros(size, dtype=np.float32)
         self.ret_buf = np.zeros(size, dtype=np.float32)
         self.val_var_buf = np.zeros(size, dtype=np.float32)
         self.cadv_buf = np.zeros(size, dtype=np.float32)    # cost advantage
-        self.cadv_var_buf = np.zeros(size, dtype=np.float32)    # cost advantage
+        self.cret_var_buf = np.zeros(size, dtype=np.float32)    # cost advantage
         self.croll_lengths_buf = np.zeros(size, dtype=np.float32)  
         self.cost_buf = np.zeros(size, dtype=np.float32)    # costs
         self.cret_buf = np.zeros(size, dtype=np.float32)    # cost return
@@ -120,13 +120,13 @@ class CPOBuffer:
         # bit memory inefficient, but more convenient, to store next_obs
         self.nextobs_archive = np.zeros(combined_shape(archive_size, self.obs_shape), dtype=np.float32)
         self.adv_archive = np.zeros(archive_size, dtype=np.float32)
-        self.adv_var_archive = np.zeros(archive_size, dtype=np.float32)
+        self.ret_var_archive = np.zeros(archive_size, dtype=np.float32)
         self.rew_archive = np.zeros(archive_size, dtype=np.float32)
         self.ret_archive = np.zeros(archive_size, dtype=np.float32)
         self.val_archive = np.zeros(archive_size, dtype=np.float32)
         self.val_var_archive = np.zeros(archive_size, dtype=np.float32)
         self.cadv_archive = np.zeros(archive_size, dtype=np.float32)    # cost advantage
-        self.cadv_var_archive = np.zeros(archive_size, dtype=np.float32)    # cost advantage
+        self.cret_var_archive = np.zeros(archive_size, dtype=np.float32)    # cost advantage
         self.cost_archive = np.zeros(archive_size, dtype=np.float32)    # costs
         self.cret_archive = np.zeros(archive_size, dtype=np.float32)    # cost return
         self.cval_archive = np.zeros(archive_size, dtype=np.float32)    # cost value
@@ -180,7 +180,7 @@ class CPOBuffer:
 
         # self.adv_buf[path_slice] = discount_cumsum(deltas, self.gamma, self.lam)
         self.adv_buf[path_slice] = discount_cumsum(deltas, self.gamma, self.lam, weights=inv_vars)
-        self.adv_var_buf[path_slice] = discount_cumsum_weighted(self.val_var_buf[path_slice], self.lam, weights=inv_vars)
+        self.ret_var_buf[path_slice] = discount_cumsum_weighted(self.val_var_buf[path_slice], self.lam, weights=inv_vars)
         self.roll_lengths_buf[path_slice] = discount_cumsum_weighted(np.arange(self.ptr), self.lam, weights=inv_vars) - np.arange(self.ptr)
 
         # self.ret_buf[path_slice] = discount_cumsum(rews, self.gamma)[:-1]
@@ -195,7 +195,7 @@ class CPOBuffer:
         inv_cvars = 1/self.cval_var_buf[path_slice]
 
         self.cadv_buf[path_slice] = discount_cumsum(cdeltas, self.cost_gamma, self.cost_lam, weights=inv_cvars)
-        self.cadv_var_buf[path_slice] = discount_cumsum_weighted(self.cval_var_buf[path_slice], self.lam, weights=inv_cvars)
+        self.cret_var_buf[path_slice] = discount_cumsum_weighted(self.cval_var_buf[path_slice], self.lam, weights=inv_cvars)
         self.croll_lengths_buf[path_slice] = discount_cumsum_weighted(np.arange(self.ptr), self.cost_lam, weights=inv_cvars) - np.arange(self.ptr)
 
         # self.cret_buf[path_slice] = discount_cumsum(costs, self.cost_gamma)[:-1]
@@ -235,10 +235,10 @@ class CPOBuffer:
         self.logp_archive[arch_slice] = self.logp_buf[buf_slice]
         self.term_archive[arch_slice] = self.term_buf[buf_slice]
         self.adv_archive[arch_slice] = self.adv_buf[buf_slice]
-        self.adv_var_archive[arch_slice] = self.adv_var_buf[buf_slice]
+        self.ret_var_archive[arch_slice] = self.ret_var_buf[buf_slice]
         self.ret_archive[arch_slice] = self.ret_buf[buf_slice]
         self.cadv_archive[arch_slice] = self.cadv_buf[buf_slice]
-        self.cadv_var_archive[arch_slice] = self.cadv_var_buf[buf_slice]
+        self.cret_var_archive[arch_slice] = self.cret_var_buf[buf_slice]
         self.cret_archive[arch_slice] = self.cret_buf[buf_slice]
         self.epoch_archive[arch_slice] = self.epoch_buf[buf_slice]
 
@@ -266,8 +266,8 @@ class CPOBuffer:
         # Center, but do NOT rescale advantages for cost gradient
         cadv_mean, _ = mpi_statistics_scalar(self.cadv_buf)
         self.cadv_buf -= cadv_mean
-        res = [self.obs_buf, self.act_buf, self.adv_buf,
-                self.cadv_buf, self.ret_buf, self.cret_buf, 
+        res = [self.obs_buf, self.act_buf, self.adv_buf, self.ret_var_buf,
+                self.cadv_buf, self.cret_var_buf, self.ret_buf, self.cret_buf, 
                 self.logp_buf, self.val_buf, self.cval_buf,
                 self.cost_buf] \
                 + values_as_sorted_list(self.pi_info_bufs)
@@ -275,13 +275,17 @@ class CPOBuffer:
         ##### diagnostics        
         ret_mean = self.ret_buf.mean()
         cret_mean = self.cret_buf.mean()
-        norm_adv_var_mean = np.mean(self.adv_var_buf)/np.var(self.adv_buf)
-        norm_cadv_var_mean = np.mean(self.cadv_var_buf)/np.var(self.cadv_buf)
+        val_var_mean = self.val_var_buf.mean()
+        cval_var_mean = self.cval_var_buf.mean()
+        norm_adv_var_mean = np.mean(self.ret_var_buf)/np.var(self.adv_buf)
+        norm_cadv_var_mean = np.mean(self.cret_var_buf)/np.var(self.cadv_buf)
         avg_horizon_r = np.mean(self.roll_lengths_buf)
         avg_horizon_c = np.mean(self.croll_lengths_buf)
 
         diagnostics = dict(poolr_ret_mean=ret_mean, \
                             poolr_cret_mean=cret_mean, 
+                            poolr_val_var_mean = val_var_mean,
+                            poolr_cval_var_mean = cval_var_mean,
                             poolr_norm_adv_var = norm_adv_var_mean, 
                             poolr_norm_cadv_var = norm_cadv_var_mean,
                             poolr_avg_Horizon_rew = avg_horizon_r,

softlearning/replay_pools/modelbuffer.py
@@ -48,7 +48,7 @@ class ModelBuffer(CPOBuffer):
         self.act_buf = np.zeros(act_buf_shape, dtype=np.float32)
         self.nextobs_buf = np.zeros(obs_buf_shape, dtype=np.float32)
         self.adv_buf = np.zeros(scalar_shape, dtype=np.float32)
-        self.adv_var_buf = np.zeros(scalar_shape, dtype=np.float32)
+        self.ret_var_buf = np.zeros(scalar_shape, dtype=np.float32)
         self.roll_lengths_buf = np.zeros(scalar_shape, dtype=np.float32)
         self.rew_buf = np.zeros(scalar_shape, dtype=np.float32)
         self.rew_path_var_buf = np.zeros(scalar_shape, dtype=np.float32)
@@ -56,7 +56,7 @@ class ModelBuffer(CPOBuffer):
         self.val_buf = np.zeros(scalar_shape, dtype=np.float32)
         self.val_var_buf = np.zeros(scalar_shape, dtype=np.float32)
         self.cadv_buf = np.zeros(scalar_shape, dtype=np.float32)    # cost advantage
-        self.cadv_var_buf = np.zeros(scalar_shape, dtype=np.float32)   
+        self.cret_var_buf = np.zeros(scalar_shape, dtype=np.float32)   
         self.croll_lengths_buf = np.zeros(scalar_shape, dtype=np.float32)   
         self.cost_buf = np.zeros(scalar_shape, dtype=np.float32)    # costs
         self.cost_path_var_buf = np.zeros(scalar_shape, dtype=np.float32)    # costs
@@ -152,15 +152,9 @@ class ModelBuffer(CPOBuffer):
             
             rews = np.append(self.rew_buf[finish_mask, path_slice], last_val[..., np.newaxis], axis=1)
             vals = np.append(self.val_buf[finish_mask, path_slice], last_val[..., np.newaxis], axis=1)
-
-            rew_vars = self.rew_path_var_buf[finish_mask, path_slice]-\
-                        np.append(np.zeros(shape=(len(rews),1)), self.rew_path_var_buf[finish_mask, path_slice], axis=-1)[...,:-1]
-            rew_vars = np.append(rew_vars, last_val_var[..., np.newaxis], axis=1)
-            val_vars = np.append(self.val_var_buf[finish_mask, path_slice], last_val_var[..., np.newaxis], axis=1)
             
             #### inv var:
             deltas = rews[:,:-1] + self.gamma * vals[:, 1:] - vals[:, :-1]
-            delta_vars = rew_vars[:,:-1] + val_vars[:, 1:] - val_vars[:, :-1]
 
             r_ret_vars = (self.rew_path_var_buf[finish_mask, path_slice] + self.val_var_buf[finish_mask, path_slice])
             r_vars_inv = 1/(r_ret_vars+EPS)
@@ -171,7 +165,7 @@ class ModelBuffer(CPOBuffer):
             adv_vars = discount_cumsum_weighted(r_ret_vars, self.lam, weights=r_vars_inv, axis=1)
             self.roll_lengths_buf[finish_mask, path_slice] = discount_cumsum_weighted(np.arange(self.ptr), self.lam, weights=r_vars_inv, axis=1)\
                                                                 - np.arange(self.ptr)
-            self.adv_var_buf[finish_mask, path_slice] = adv_vars
+            self.ret_var_buf[finish_mask, path_slice] = adv_vars
 
             # deltas = rews[:,:-1] + self.gamma * vals[:, 1:] - vals[:, :-1]
             # self.adv_buf[finish_mask, path_slice] = discount_cumsum(deltas, self.gamma, self.lam, axis=1)
@@ -182,14 +176,8 @@ class ModelBuffer(CPOBuffer):
             costs = np.append(self.cost_buf[finish_mask, path_slice], last_cval[..., np.newaxis], axis=1)
             cvals = np.append(self.cval_buf[finish_mask, path_slice], last_cval[..., np.newaxis], axis=1)
 
-            cost_vars = self.cost_path_var_buf[finish_mask, path_slice]-\
-                            np.append(np.zeros(shape=(len(costs), 1)),self.cost_path_var_buf[finish_mask, path_slice], axis=-1)[...,:-1]
-            cost_vars = np.append(cost_vars, last_cval_var[..., np.newaxis], axis=1)
-            cval_vars = np.append(self.cval_var_buf[finish_mask, path_slice], last_cval_var[..., np.newaxis], axis=1)
-
             # self.cadv_buf[finish_mask, path_slice] = discount_cumsum(cdeltas, self.cost_gamma, self.cost_lam, axis=1)
             cdeltas = costs[:, :-1] + self.gamma * cvals[:, 1:] - cvals[:, :-1]
-            cdelta_vars = cost_vars[:, :-1] + 1.0 * cval_vars[:, 1:] - cval_vars[:, :-1]
             
             cret_vars = (self.cost_path_var_buf[finish_mask, path_slice] + self.cval_var_buf[finish_mask, path_slice])
             c_vars_inv = 1/(cret_vars+EPS)
@@ -200,7 +188,7 @@ class ModelBuffer(CPOBuffer):
             cadv_vars = discount_cumsum_weighted(cret_vars, self.cost_lam, weights=c_vars_inv, axis=1)
             self.croll_lengths_buf[finish_mask, path_slice] = discount_cumsum_weighted(np.arange(self.ptr), self.cost_lam, weights=c_vars_inv, axis=1) \
                                              - np.arange(self.ptr)
-            self.cadv_var_buf[finish_mask, path_slice] = cadv_vars
+            self.cret_var_buf[finish_mask, path_slice] = cadv_vars
             # self.cret_buf[finish_mask, path_slice] = discount_cumsum(costs, self.cost_gamma, axis=1)[:,:-1]
             self.cret_buf[finish_mask, path_slice] = self.cadv_buf[finish_mask, path_slice] + self.cval_buf[finish_mask, path_slice]
 
@@ -258,20 +246,25 @@ class ModelBuffer(CPOBuffer):
                 
                 ret_mean = self.ret_buf[self.populated_mask].mean()
                 cret_mean = self.cret_buf[self.populated_mask].mean()
-                norm_adv_var_mean = np.mean(self.adv_var_buf[self.populated_mask])/np.var(self.adv_buf[self.populated_mask])
-                norm_cadv_var_mean = np.mean(self.cadv_var_buf[self.populated_mask])/np.var(self.cadv_buf[self.populated_mask])
+                val_var_mean = self.val_var_buf[self.populated_mask].mean()
+                cval_var_mean = self.cval_var_buf[self.populated_mask].mean()
+                norm_adv_var_mean = np.mean(self.ret_var_buf[self.populated_mask])/np.var(self.adv_buf[self.populated_mask])
+                norm_cadv_var_mean = np.mean(self.cret_var_buf[self.populated_mask])/np.var(self.cadv_buf[self.populated_mask])
                 avg_horizon_r = np.mean(self.roll_lengths_buf[self.populated_mask])
                 avg_horizon_c = np.mean(self.croll_lengths_buf[self.populated_mask])
             else:
                 ret_mean = 0
                 cret_mean = 0
-                norm_adv_var_mean = 0
-                norm_cadv_var_mean = 0
+                val_var_mean = 0
+                cval_var_mean = 0
+                norm_ret_var_mean = 0
+                norm_cret_var_mean = 0
                 avg_horizon_r = 0
                 avg_horizon_c = 0
 
-            res = [self.obs_buf, self.act_buf, self.adv_buf,
-                    self.cadv_buf, self.ret_buf, self.cret_buf, 
+
+            res = [self.obs_buf, self.act_buf, self.adv_buf, self.ret_var_buf,
+                    self.cadv_buf, self.cret_var_buf, self.ret_buf, self.cret_buf, 
                     self.logp_buf, self.val_buf, self.cval_buf,
                     self.cost_buf] \
                     + values_as_sorted_list(self.pi_info_bufs)
@@ -280,6 +273,8 @@ class ModelBuffer(CPOBuffer):
             res = [buf[self.populated_mask] for buf in res]
             diagnostics = dict(poolm_ret_mean=ret_mean, \
                                 poolm_cret_mean=cret_mean, 
+                                poolm_val_var_mean = val_var_mean,
+                                poolm_cval_var_mean = cval_var_mean,
                                 poolm_norm_adv_var = norm_adv_var_mean, 
                                 poolm_norm_cadv_var = norm_cadv_var_mean,
                                 poolm_avg_Horizon_rew = avg_horizon_r,