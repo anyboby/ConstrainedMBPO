fixed more sampler diagnostics and added variance acceleration as terminal condition

mbpo/models/fake_env.py
@@ -86,7 +86,7 @@ class FakeEnv:
                                         out_dim=1,
                                         loss=self.cost_m_loss,
                                         name='CostNN',
-                                        hidden_dims=(128,128,128),
+                                        hidden_dims=(128,128),
                                         lr=8e-5,
                                         # lr_decay=0.96,
                                         # decay_steps=10000, 
@@ -222,7 +222,7 @@ class FakeEnv:
                 inputs_cost = next_obs
 
             costs, cost_var = self._cost_model.predict(inputs_cost, factored=False, inc_var=True)
-            cost_var = np.mean(cost_var, axis=0) + np.mean(costs**2, axis=0) - (np.mean(costs, axis=0))**2
+            cost_var = (np.mean(cost_var, axis=0) + np.mean(costs**2, axis=0) - (np.mean(costs, axis=0))**2)[...,0]
 
             # if self.inc_var_c:
             #     costs, cost_var = self._cost_model.predict(inputs_cost, factored=True, inc_var=True)

softlearning/replay_pools/modelbuffer.py
@@ -75,12 +75,13 @@ class ModelBuffer(CPOBuffer):
         # terminated_paths_mask essentially notes the same thing as populated_mask but is one_dimensional for 
         #   convenience
 
-        self.ptr, self.path_start_idx, self.max_size, self.populated_mask, self.terminated_paths_mask = \
-                                                                                0, \
-                                                                                0, \
-                                                                                np.ones(self.batch_size)*self.max_path_length, \
-                                                                                np.zeros((self.batch_size, self.max_path_length), dtype=np.bool), \
-                                                                                np.zeros(self.batch_size, dtype=np.bool)
+        self.ptr, self.path_start_idx, self.max_size, self.populated_mask, self.populated_indices, self.terminated_paths_mask = \
+                                                            0, \
+                                                            0, \
+                                                            np.ones(self.batch_size)*self.max_path_length, \
+                                                            np.zeros((self.batch_size, self.max_path_length), dtype=np.bool), \
+                                                            np.repeat(np.arange(self.max_path_length)[None], axis=0, repeats=self.batch_size), \
+                                                            np.zeros(self.batch_size, dtype=np.bool)
 
 
     @property
@@ -262,6 +263,21 @@ class ModelBuffer(CPOBuffer):
                 discount_cumsum_weighted(np.arange(self.ptr), 1.0, c_weight_mat)*c_weight_norm - np.arange(self.ptr)
             #### R_t = A_GAE,t^iv + V_t
             self.cret_buf[finish_mask, path_slice] = self.cadv_buf[finish_mask, path_slice] + self.cval_buf[self.model_ind, finish_mask, path_slice]
+            
+            #=====================================================================#
+            #  Determine Rollout Lengths                                          #
+            #=====================================================================#
+            
+            ### calc variance acceleration and normalize
+            c_var_acc = np.diff(np.diff(self.cret_var_buf[finish_mask, path_slice], axis=-1), axis=-1)
+            c_var_acc = (c_var_acc-np.mean(c_var_acc, axis=-1)[...,None])/(np.std(c_var_acc, axis=-1)[...,None]+EPS)
+            v_var_acc = np.diff(np.diff(self.ret_var_buf[finish_mask, path_slice], axis=-1), axis=-1)
+            v_var_acc = (v_var_acc-np.mean(v_var_acc, axis=-1)[...,None])/(np.std(v_var_acc, axis=-1)[...,None]+EPS)
+
+            var_acc_threshold = 1 ### all variance accelerations above 1 std are filtered out
+            var_acc_mask = np.logical_or(c_var_acc>var_acc_threshold, v_var_acc>var_acc_threshold)
+            horizons = np.argmax(var_acc_mask, axis=-1)[...,None]
+            self.populated_mask[finish_mask,:] = self.populated_indices[finish_mask,:]<horizons
 
         # mark terminated paths
         self.terminated_paths_mask += finish_mask

softlearning/samplers/model_sampler.py
@@ -109,8 +109,8 @@ class ModelSampler(CpoSampler):
         ensemble_cost_var_perstep = self._total_cost_var/(self._total_samples+EPS)
         ensemble_dyn_var_perstep = self._total_dyn_var/(self._total_samples+EPS)
 
-        ensemble_cost_rate = np.sum(np.mean(self._path_cost, axis=0))(self._total_samples+EPS)
-        ensemble_rew_rate = np.sum(np.mean(self._path_return, axis=0))(self._total_samples+EPS)
+        ensemble_cost_rate = np.sum(np.mean(self._path_cost, axis=0))/(self._total_samples+EPS)
+        ensemble_rew_rate = np.sum(np.mean(self._path_return, axis=0))/(self._total_samples+EPS)
 
         vals_mean = self._total_Vs / (self._total_samples+EPS)
         vals_var = self._total_V_var / (self._total_samples+EPS)
@@ -249,7 +249,6 @@ class ModelSampler(CpoSampler):
         next_obs, reward, terminal, info = self.env.step(current_obs, a)
 
         reward = np.squeeze(reward, axis=-1)
-        
         rew_var = info.get('rew_ensemble_var', np.zeros(reward.shape))
 
         c = np.squeeze(info.get('cost', np.zeros(reward.shape)))
@@ -262,7 +261,6 @@ class ModelSampler(CpoSampler):
         dyn_var = info.get('dyn_ensemble_var', np.zeros(reward.shape))
 
         next_val, next_val_var = self.policy.get_v(next_obs, factored=True, inc_var=True)
-
         next_cval, next_cval_var = self.policy.get_vc(next_obs, factored=True, inc_var=True)
 
         #### variance for gaussian mixture, add dispersion of means to variance
@@ -276,6 +274,9 @@ class ModelSampler(CpoSampler):
 
         ### check if too uncertain before storing info of the taken step 
         ### (so we don't take a "bad step" by appending values of next state)
+        ep_cval_var = np.var(next_cval, axis=0)
+        ep_val_var = np.var(next_val, axis=0)
+
         cost_uncertainty = self._path_cost_var[alive_paths] + c_var
         rew_uncertainty = self._path_return_var[alive_paths] + rew_var 
 
@@ -283,7 +284,7 @@ class ModelSampler(CpoSampler):
         cost_var_rm = self._total_cost_var+EPS**2/(self._total_samples+EPS)
         rew_var_rm = self._total_rew_var+EPS**2/(self._total_samples+EPS)
 
-        # too_uncertain_mask = path_uncertainty > self._max_uncertainty
+        ## epistemic trajectory-return variance vs epistemic value variance as termination
         too_uncertain_paths = np.logical_or(cost_uncertainty + next_cval_var > self._max_uncertainty_c, \
                                             rew_uncertainty + next_val_var > self._max_uncertainty_rew) 
         # too_uncertain_mask_path = np.logical_or(cost_uncertainty > next_cval_var, \