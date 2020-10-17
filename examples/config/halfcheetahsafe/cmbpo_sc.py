79a928a4dc472943e0e73ca395cd08e423463bb5
anyboby <zanger.moritz@googlemail.com>
2020-10-17 14:10:46 +0200

    params hcs sc

--- examples/config/halfcheetahsafe/cmbpo_sc.py
+++ examples/config/halfcheetahsafe/cmbpo_sc.py
@@ -45,7 +45,7 @@ params = {
         'dyn_model_train_schedule': [50, 100, 1, 1],
         'cost_model_train_schedule': [25, 80, 1, 1],
         'cares_about_cost': True,
-        'm_sampling_discount': 0.997,           
+        'm_sampling_discount': 0.995,           
         'max_uncertainty_c' :4.0,              ### only applies if rollout_mode=='iv_gae' or rollout_mode=='uncertainty'
         'max_uncertainty_rew' : 3.5,
         'rollout_mode' : 'schedule',           #### choose from 'iv_gae', 'schedule', or 'uncertainty'
@@ -54,7 +54,7 @@ params = {
                                                     # range of (min_epoch, max_epoch)
                                                     ### Only applies if rollout_mode=='schedule'
         'maxroll': 8,      ### only really relevant for iv gae
-        'max_tddyn_err' : 0.03,
+        'max_tddyn_err' : 0.02,
         'max_tddyn_err_decay' : .9999,
         'batch_size_policy': 10000,              ### how many samples 
         'min_real_samples_per_epoch': 100,
